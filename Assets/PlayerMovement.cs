12e1bdb6c8b2d24809e3348a60bca19f57bc27fe
Kubzone <crower9@onet.pl>
2024-06-12 11:05:37 +0200

    added shoot, jump and fire

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -8,19 +8,33 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] Rigidbody2D rb;
     [SerializeField] Transform groundCheck;
     [SerializeField] LayerMask groundLayer;
+    [SerializeField] GameObject shootPrefab;
+    [SerializeField] Transform bulletSpawn;
 
     [SerializeField] float horizontal;
+    [SerializeField] float vertical;
     [SerializeField] float speed = 50f;
     [SerializeField] float jumpPower = 16f;
+    [SerializeField] float shootForce = 10f; // Force for the shoot
     bool isFacingRight = true;
-
+    bool isJumping = false;
+    public float thrust = 1.0f;
     // Start is called before the first frame update
-    void Start()
+    void Start()    
     {
 
     }
     void Update()
     {
+        
+        
+        if (isJumping && IsGrounded())
+        {
+            print("jump Detected11");
+            rb.AddForce(new Vector2(0f, jumpPower), ForceMode2D.Impulse);
+            //rb.velocity = new Vector2(vertical * speed, rb.velocity.y);
+            isJumping = false;
+        }
         rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
     }
     public void Move(InputAction.CallbackContext context)
@@ -28,6 +42,32 @@ public class PlayerMovement : MonoBehaviour
         print("Move Detected");
         horizontal = context.ReadValue<Vector2>().x;
     }
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            print("jump Detected");
+            isJumping = true;
+        }
+    }
+    bool IsGrounded()
+    {
+        Collider2D groundCheckCollider = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+        return groundCheckCollider != null;
+    }
+    public void Fire(InputAction.CallbackContext context)
+    {
+        
+        if (context.performed)
+        {
+            GameObject shoot = Instantiate(shootPrefab, bulletSpawn.position, Quaternion.identity);
+            Rigidbody2D shootRb = shoot.GetComponent<Rigidbody2D>();
+            shootRb.AddForce(new Vector2(shootForce, 0), ForceMode2D.Impulse);
+            //shootRb.AddForce(new Vector2(10f,0f));
+            
+        }
+    }
+
 }
  /*   // Update is called once per frame
     void Update()
